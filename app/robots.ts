75818a54146a4541715a0f044564ac56aec4ecbb
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2026-04-24 16:28:44 +0000

    feat: update robots.txt structure to use array for rules

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,16 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'AdsBot-Google',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://suntagandtitle.com/sitemap.xml',
   };
 }
