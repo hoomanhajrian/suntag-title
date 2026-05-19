9977132bc25230fa60fac21b2fd0387e622cdee4
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2026-05-19 17:02:33 +0000

    fix(Google Tag Manager): update GTM ID from WL5LS2BC to T8ST2J2G

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,14 +84,14 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WL5LS2BC');`}
+})(window,document,'script','dataLayer','GTM-T8ST2J2G');`}
         </Script>
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WL5LS2BC"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T8ST2J2G"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
